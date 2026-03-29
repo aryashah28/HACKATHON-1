c169234c3f65b38868bf57b9d3df052830158a08
PremNayak544 <premnayak544@gmail.com>
2026-03-29 12:31:19 +0530

    done

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,6 +1,18 @@
-def extract(file):
-    return {
-        "amount": 100,
-        "description": "Auto OCR",
-        "date": "2025-01-01"
-    }
+﻿from fastapi import APIRouter, Depends, HTTPException
+from database.db import get_db
+from schemas.user_schema import UserLogin, TokenResponse
+from services.auth_service import signup, login
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/signup", response_model=TokenResponse)
+def signup_user(data: UserLogin, db=Depends(get_db)):
+    token = signup(db, data.username, data.password)
+    return {"access_token": token}
+
+@router.post("/login", response_model=TokenResponse)
+def login_user(data: UserLogin, db=Depends(get_db)):
+    token = login(db, data.username, data.password)
+    if token is None:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    return {"access_token": token}
