1c04802226c2780c3095313e278678155ecb1f6d
PremNayak544 <premnayak544@gmail.com>
2026-03-29 12:29:44 +0530

    done

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from database.db import Base, engine
 
-from routes import auth_routes, expense_routes, approval_routes, rule_routes
+from routes import auth_routes, expense_routes, approval_routes, rule_routes, user_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth_routes.router)
+app.include_router(user_routes.router)
 app.include_router(expense_routes.router)
 app.include_router(approval_routes.router)
 app.include_router(rule_routes.router)
